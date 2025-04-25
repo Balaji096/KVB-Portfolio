a99edfec1625699d9fca5e3ba1460a3e489c65ba
K. Venkata Balaji <balaji.kanthuri22@gmail.com>
2025-04-25 16:00:18 +0530

    page.js

--- app/page.js
+++ app/page.js
@@ -7,36 +7,55 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import { useEffect, useState } from "react";
 
 async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
 
   const data = await res.json();
-
   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
 
   return filtered;
-};
+}
 
-export default async function Home() {
-  const blogs = await getData();
+export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+  const [isClient, setIsClient] = useState(false);
+
+  // Fetch blog data on the client-side only
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const fetchedBlogs = await getData();
+        setBlogs(fetchedBlogs);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchBlogs();
+
+    // Set isClient to true after the first render to enable client-side code
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // or a loading spinner
+  }
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
       <Skills />
       <Projects />
       <Education />
-      {/* <Blog blogs={blogs} /> */}
+     {/* <Blog blogs={blogs} />  Uncomment this once data is fetched */}
       <ContactSection />
     </div>
-  )
+  );
 };
-
-
